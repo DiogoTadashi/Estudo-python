7d970d3c869dfa35985e6d34d1441f823ca0b638
DiogoTadashi <diogotadashi22@gmail.com>
2026-08-13 18:00:08 -0300

    add more python exercises

--- pythonProject/Desafio 086.py
+++ pythonProject/Desafio 086.py
@@ -2,3 +2,15 @@
 Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 '''
 
+matriz = [[], [], []]
+
+for linha in range(3):
+    for coluna in range(3):
+        valor = int(input(f"Digite um valor para [{linha}, {coluna}]: "))
+        matriz[linha].append(valor)
+        
+print("Matriz 3x3:")
+for linha in matriz:
+    for elem in linha:
+        print(f"[{elem: ^5}]", end="")
+    print()
--- pythonProject/Desafio 087.py
+++ pythonProject/Desafio 087.py
@@ -4,3 +4,34 @@ A) A soma de todos os valores pares digitados.
 B) A soma dos valores da terceira coluna.
 C) O maior valor da segunda linha.
 '''
+
+matriz = [[], [], []]
+soma = 0
+soma3 = 0
+
+for linha in range(3):
+    for coluna in range(3):
+        valor = int(input(f"Digite um valor para [{linha}, {coluna}]: "))
+        matriz[linha].append(valor)
+    
+for linha in matriz:
+    for elemPar in linha:
+        if elemPar % 2 == 0:
+            soma += elemPar
+            
+for linha in matriz:      
+    soma3 += linha[2]
+
+maiorV = max(matriz[1])
+
+print("Matriz 3x3:")
+for linha in matriz:
+    for elem in linha:
+        print(f"[{elem: ^5}]", end="")
+    print()
+
+print(f"\nA soma dos elementos pares é {soma}")
+
+print(f"A soma dos elementos da terceira coluna é {soma3}")
+
+print(f"O maior valor da segunda linha é {maiorV}")
